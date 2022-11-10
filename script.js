da5e88c302191ad9c58290b537d227c3b461ca0b
tracey <tracey.d.huang@gmail.com>
2022-11-10 20:11:23 +1100

    Add button and input search bar

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const CITY = 'chiang mai'
 const KEY = '07b455e1971cd1f6a070f7d09f4e2d4c'
 
+const searchButton = document.querySelector('button');
+searchButton.addEventListener('click', () => {
+    let input = document.getElementById('city').value;
+    console.log(input)
+})
+
+
+
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${KEY}&units=metric`)
     .then(response => {
         return response.json();
@@ -12,3 +20,4 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${KEY}&un
         console.log(response.main.temp)
         console.log(response.weather[0].description)
     })
+
